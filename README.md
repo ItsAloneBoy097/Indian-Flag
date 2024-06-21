8b73a4f86f568fbfc5741ddfcaf95a8ba93adf4d
ItsAloneBoy <itsaloneboy097@gmail.com>
2024-06-21 21:56:24 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# Indian-Flag
+
+YouTube - https://youtube.com/@LuckyCoderYT
+
+Discord - https://dsc.gg/luckycoder
+
+GitHub - https://github.com/ItsAloneBoy097
---
+++ main.py
@@ -0,0 +1,54 @@
+import turtle
+
+flag = turtle.Turtle()
+flag.speed(3)
+flag.pensize(5)
+flag.pencolor('#054187')
+
+
+def draw(x, y):
+    flag.penup()
+    flag.goto(x, y)
+    flag.pendown()
+
+
+
+for i in range(24):
+    flag.forward(80)
+    flag.backward(80)
+    flag.left(15)
+draw(0, -80)
+flag.circle(80, 360)
+
+
+flag.color("green")
+flag.begin_fill()
+
+flag.forward(350)
+flag.backward(700)
+flag.right(90)
+flag.forward(200)
+flag.left(90)
+flag.forward(700)
+flag.left(90)
+flag.forward(200)
+flag.left(90)
+
+flag.end_fill()
+
+flag.color('orange')
+draw(-350, 80)
+flag.begin_fill()
+
+flag.right(180)
+flag.forward(700)
+flag.left(90)
+flag.forward(200)
+flag.left(90)
+flag.forward(700)
+flag.left(90)
+flag.forward(200)
+
+flag.end_fill()
+
+turtle.done()
